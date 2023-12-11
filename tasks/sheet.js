d2f555ebebddfcde554b3face60ad8b546fb9b11
Johnny Richardson <johnny.c.rich@gmail.com>
2023-12-11 18:21:01 -0500

    refactor: use of service account for experimental parse of forms sheets.

--- tasks/sheet.js
+++ tasks/sheet.js
@@ -1,72 +1,19 @@
-import  fs from "fs/promises"
-import  path from "path";
+import path from "path";
 import process from 'process';
-import { authenticate } from "@google-cloud/local-auth";
-// const { google } = require("googleapis");
-
 import { google } from "googleapis";
 
-// const base64 = require("js-base64").Base64;
-// import * as OAuth2Data from "./tcredentials.json" assert { type: "json" };
-
-// If modifying these scopes, delete token.json.
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), "token.json");
 const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
 /**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-	try {
-		const content = await fs.readFile(TOKEN_PATH);
-		const credentials = JSON.parse(content);
-		return google.auth.fromJSON(credentials);
-	} catch (err) {
-		return null;
-	}
-}
-
-/**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-	const content = await fs.readFile(CREDENTIALS_PATH);
-	const keys = JSON.parse(content);
-	const key = keys.installed || keys.web;
-	const payload = JSON.stringify({
-		type: "authorized_user",
-		client_id: key.client_id,
-		client_secret: key.client_secret,
-		refresh_token: client.credentials.refresh_token
-	});
-	await fs.writeFile(TOKEN_PATH, payload);
-}
-
-/**
- * Load or request or authorization to call APIs.
- *
+ * request or authorization to call APIs.
  */
 async function authorize() {
-	let client = await loadSavedCredentialsIfExist();
-	if (client) {
-		return client;
-	}
-	client = await authenticate({
-		scopes: SCOPES,
-		keyfilePath: CREDENTIALS_PATH
-	});
-	if (client.credentials) {
-		await saveCredentials(client);
-	}
+    const client = new google.auth.GoogleAuth({
+			keyFile: CREDENTIALS_PATH,
+			scopes: SCOPES
+		});
+		
 	return client;
 }
 
